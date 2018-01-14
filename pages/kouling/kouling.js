9887a9bef8873133cb36bfceb8194c5fa062adb6
liujiankang <liujiankang1@foxmail.com>
2018-01-14 20:58:06 +0800

    修改底部tab，增加倾听红包录音播放

---
+++ pages/kouling/kouling.js
@@ -0,0 +1,99 @@
+//kouling.js
+//获取应用实例
+const app = getApp()
+const recorderManager = wx.getRecorderManager()
+
+Page({
+  data: {
+    houBaoStyle: 1,
+    userInfo: [],
+    shuoming1: '小伙伴们说对口令就能获得随即打赏',
+    shuoming2: '好友听完你说的话就能领取赏金',
+    kouling: '',
+    Money: '',
+    Number: '',
+    fuwufee: 0.0,
+    hasRecord: false,
+    recording: false,
+    playing: false,
+  },
+
+  //事件处理函数
+  ChangeTab: function (e) {
+    var that = this;
+    console.log("currentTab")
+    var currentTab = e.currentTarget.dataset.id;
+    that.setData({
+      houBaoStyle: currentTab,
+    })
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getUserInfo({
+      success: function (user) {
+        console.log(user)
+        that.setData({
+          userInfo: user.userInfo,
+        })
+      }
+    })
+  },
+
+  // 跳转链接
+
+  tomyRecord: function () {
+    var that = this;
+    wx.navigateTo({
+      url: './mRecord/myRecord',
+    })
+
+  },
+  // 获取页面填入的值
+  koulingInput: function (e) {
+    var that = this;
+    console.log(e.detail.value)
+    that.setData({
+      kouling: e.detail.value,
+    })
+  },
+  MoneyInput: function (e) {
+    var that = this;
+    console.log(e.detail.value)
+    that.setData({
+      Money: e.detail.value,
+      fuwufee: e.detail.value * 0.02,
+    })
+  },
+  NumberInput: function (e) {
+    var that = this;
+    console.log(e.detail.value)
+    that.setData({
+      Number: e.detail.value,
+    })
+  },
+
+  toShare: function (e) {
+    var that = this;
+    console.log(e.detail.value)
+    wx.request({
+      url: "http://www.baidu.test",
+      data: {
+        kouling: this.data.kouling,
+        money: this.data.Money,
+        count: this.data.Number,
+        fuwufei: this.data.fuwufee,
+      },
+    })
+    console.log(e.detail.value);
+  },
+
+  huatong: function () {
+    wx.startRecord({
+
+    })
+  },
+  stoprecord: function () {
+    wx.stopRecord();
+  }
+
+})
