fe4cf8f25b05d2b69290363bab7acd7e03d4c37c
zhouxiaolong <zhouxiaolong@bjhtgy.cn>
2018-01-15 22:52:08 +0800

    1，整理项目文件组织结构 2，实现检验登录状态

--- app.js
+++ app.js
@@ -1,54 +1,74 @@
 //app.js
 App({
+  globalData: {
+    userInfo: [],
+    sessionInfo: ''
+  },
   onLoad:function(){
     console.log("App onLoad")
-    
+  },
+  checkSession: function (cb){
+    var that = this
+    var isLogin=false;
+    wx.checkSession({
+      success: function () {
+        //session 未过期，并且在本生命周期一直有效
+        isLogin=true
+        that.globalData.sessionInfo = wx.getStorageSync('3rd_session') || []
+        wx.getUserInfo({
+          success: function (user) {
+            that.globalData = user.userInfo
+          }
+        })
+        cb&&typeof cb.success == "function" && cb.success()
+      },
+      fail: function () {
+        //登录态过期
+        //重新登录
+        // 登录
+        wx.login({
+          success: res => {
+            // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            if (res.code) {
+              //发起网络请求
+              wx.request({
+                url: 'https://test.com/onLogin',
+                data: {
+                  code: res.code
+                },
+                success: function (res) {
+                  isLogin=true
+                  that.globalData.session=res.data
+                  wx.setStorageSync('3rd_session', res.data)
+                  wx.getUserInfo({
+                    success: function (user) {
+                      that.globalData = user.userInfo
+                    }
+                  })
+                  cb && typeof cb.success == "function" && cb.success()
+                  }
+                 ,
+                fail:function(res){
+
+                }
+              })
+            } else {
+              console.log('获取用户登录态失败！' + res.errMsg)
+            }
+          }
+        })
+      }
+    })
+    console.log(isLogin)
   },
   onLaunch: function () {
     var that = this
     console.log("App OnLaunch")
-    wx.getUserInfo({
-      success: function (user) {
-        // console.log(user)
-        // this.setglobalData({
-        //   userInfo :user.userInfo
-        // })
-        that.globalData.userInfo = user.userInfo
-      }
-    })
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    //检查登录状态
+    that.checkSession({success:function(){
+      console.log('获取用户登录态成功！')
+    }})
+    //请求成功
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: []
   }
 })
