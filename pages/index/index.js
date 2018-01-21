db4b6686583d6539ac19f345767d321608a6111d
liujiankang <liujiankang1@foxmail.com>
2018-01-21 20:21:21 +0800

    改变摇摇包和倾听包位置，并重新命名文件夹

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,83 +1,75 @@
-//index.js
+//kouling.js
 //获取应用实例
 const app = getApp()
-
 var methods = require('../../utils/methods.js')
 
 Page({
   data: {
-    houBaoStyle:1,
-    userInfo:[],
-    hbType: [],
-    shuoming1: '好友听完你说的话就能领取赏金',
-    shuoming2:'小伙伴们说对口令就能获得随即打赏',
-    shuoming3: '小伙伴摇摇力超过武力值即可领红包',
-    kouling: '',
-    Money:'',
-    Number:'',
-    fuwufee:0.0,
-    hasRecord:false,
-    recording:false,
-    playing :false ,
-    filePath:'',
-    tempFilePath:'',
-    chargeParam:0,
-    dealParam:0,
-    numParam:0,
-    withdrawParam:0,
-    sn:"code",
-    rate:0.01,
-    minVal:0.01,
-    tips:'长按输入你想说的内容',
-    NetUrl:'http://www.chemchemchem.com/file/201801201744/178f12143291d5ebe3117c0639d10992.silk'
+    houBaoStyle: 1,
+    userInfo: [],
+    shuoming: '小伙伴摇摇力超过武力值即可领红包',
+    wiliset:'60',
+    Money: '',
+    Number: '',
+    fuwufee: 0.0,
+    moving: false,
+    wulizhi:0,
+    rate:1,
   },
-  
-  //事件处理函数
+
   onLoad: function () {
     var that = this;
-    var tipArray = app.globalData.hbType[0].tips;
-    console.log("tips length " + tipArray.length)
-    Math.random() * (tipArray.length-1)
-    var num = Math.round(Math.random() * (tipArray.length-1) + 0);
-    console.log("random "+num)
-    that.setData({
-      tips: tipArray[num]
-    })
-    
     wx.getUserInfo({
-      success:function(user){
+      success: function (user) {
         console.log(user)
         that.setData({
-          userInfo:user.userInfo,
+          userInfo: user.userInfo,
         })
       }
     })
   },
-  
-  // 跳转链接
 
-  tomyRecord:function(){
-    var that = this;
-    wx.navigateTo({
-      url: './mRecord/myRecord',
+  //开始摇手机
+  startMove:function(){
+      var that=this
+      that.setData({
+        moving:true
+      })
+    wx.startAccelerometer({
+      success: function (res) {
+        console.log("the wuli " + res)
+      }
     })
-
+    wx.onAccelerometerChange(function (res) {
+      // console.log(res.x+',')
+      // console.log(res.y + ',')
+      // console.log(res.z + ',')
+      var wuli = 0 + res.x * res.x + res.y * res.y + res.z * res.z
+      if (wuli > 10){
+        console.log(wuli)
+        that.setData({
+          wulizhi: wuli.toFixed(2)*that.data.rate
+        })
+      }
+     
+      
+    })
+   
   },
   // 获取页面填入的值
-  koulingInput:function(e){
+  wuliInput: function (e) {
     var that = this;
     console.log(e.detail.value)
     that.setData({
-      kouling:e.detail.value,
+      wiliset: e.detail.value,
     })
   },
-  MoneyInput:function(e){
+  MoneyInput: function (e) {
     var that = this;
-   var fuwufee=methods.getChargeFee()
     console.log(e.detail.value)
     that.setData({
       Money: e.detail.value,
-      fuwufee: fuwufee
+      fuwufee: e.detail.value * 0.02,
     })
   },
   NumberInput: function (e) {
@@ -88,11 +80,11 @@ Page({
     })
   },
 
-  toShare:function(e) {
+  toShare: function (e) {
     var that = this;
     console.log(e.detail.value)
-    if(this.data.Money==''){
-    
+    if (this.data.Money == '') {
+
       wx.showModal({
         title: '提示',
         content: '请先输入金额',
@@ -106,7 +98,7 @@ Page({
       })
     }
 
-    else if(this.data.Number==''){
+    else if (this.data.Number == '') {
       wx.showModal({
         title: '提示',
         content: '请输入红包个数',
@@ -119,65 +111,15 @@ Page({
         }
       })
     }
-    else{
-      methods.uploadFile()
-      methods.hongbaoCreate(that.data.Money,that.data.count,that.data.fuwufee)
-    wx.navigateTo({
-      url: 'Share/share',
-    })
-    console.log(e.detail.value);
-  }
+    else {
+      //停止监听武力值
+      wx.stopAccelerometer({})
+      methods.hongbaoCreate(that.data.Money, that.data.count, that.data.fuwufee,'',that.data.wuliset)
+      wx.navigateTo({
+        url: 'Share/share',
+      })
+      console.log(e.detail.value);
+    }
   },
 
-  startRecord:function(){
-    var that = this
-    console.log("stat record");
-    this.setData({
-      recording:true
-    })
-    console.log(this.data.recording);
-
-   wx.startRecord({
-     success: function (res) {
-       that.setData({
-       tempFilePath:res.tempFilePath
-     })
-     }
-   })
-
-  },
- stopRecord:function(){
-   var that=this
-   console.log("stop record");
-   this.setData({recording: false})
-    wx.stopRecord({
-
-    })
-    that.setData({
-      hasRecord:true
-    })
- },
-
-  playVoice: function () {
-   var that = this
-    var filePath=that.data.tempFilePath
-   console.log(filePath);
-   wx.playVoice({
-     filePath: filePath,
-   })
-    //that.saveFileToLocal()
-   methods.uploadFile(filePath)
-  //methods.downloadFile()
-  },
-
-  //播放网络返回的地址
-  playNetVoice:function(){
-   console.log("local file address "+this.data.filePath)
-   wx.playVoice({
-     filePath:this.data.filePath,
-   })
-  }
-  
- 
-  
 })
---
+++ pages/qingting/qingting.js
@@ -0,0 +1,183 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+var methods = require('../../utils/methods.js')
+
+Page({
+  data: {
+    houBaoStyle:1,
+    userInfo:[],
+    hbType: [],
+    shuoming1: '好友听完你说的话就能领取赏金',
+    shuoming2:'小伙伴们说对口令就能获得随即打赏',
+    shuoming3: '小伙伴摇摇力超过武力值即可领红包',
+    kouling: '',
+    Money:'',
+    Number:'',
+    fuwufee:0.0,
+    hasRecord:false,
+    recording:false,
+    playing :false ,
+    filePath:'',
+    tempFilePath:'',
+    chargeParam:0,
+    dealParam:0,
+    numParam:0,
+    withdrawParam:0,
+    sn:"code",
+    rate:0.01,
+    minVal:0.01,
+    tips:'长按输入你想说的内容',
+    NetUrl:'http://www.chemchemchem.com/file/201801201744/178f12143291d5ebe3117c0639d10992.silk'
+  },
+  
+  //事件处理函数
+  onLoad: function () {
+    var that = this;
+    var tipArray = app.globalData.hbType[0].tips;
+    console.log("tips length " + tipArray.length)
+    Math.random() * (tipArray.length-1)
+    var num = Math.round(Math.random() * (tipArray.length-1) + 0);
+    console.log("random "+num)
+    that.setData({
+      tips: tipArray[num]
+    })
+    
+    wx.getUserInfo({
+      success:function(user){
+        console.log(user)
+        that.setData({
+          userInfo:user.userInfo,
+        })
+      }
+    })
+  },
+  
+  // 跳转链接
+
+  tomyRecord:function(){
+    var that = this;
+    wx.navigateTo({
+      url: './mRecord/myRecord',
+    })
+
+  },
+  // 获取页面填入的值
+  koulingInput:function(e){
+    var that = this;
+    console.log(e.detail.value)
+    that.setData({
+      kouling:e.detail.value,
+    })
+  },
+  MoneyInput:function(e){
+    var that = this;
+   var fuwufee=methods.getChargeFee()
+    console.log(e.detail.value)
+    that.setData({
+      Money: e.detail.value,
+      fuwufee: fuwufee
+    })
+  },
+  NumberInput: function (e) {
+    var that = this;
+    console.log(e.detail.value)
+    that.setData({
+      Number: e.detail.value,
+    })
+  },
+
+  toShare:function(e) {
+    var that = this;
+    console.log(e.detail.value)
+    if(this.data.Money==''){
+    
+      wx.showModal({
+        title: '提示',
+        content: '请先输入金额',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+
+    else if(this.data.Number==''){
+      wx.showModal({
+        title: '提示',
+        content: '请输入红包个数',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+    else{
+      methods.uploadFile()
+      methods.hongbaoCreate(that.data.Money,that.data.count,that.data.fuwufee)
+    wx.navigateTo({
+      url: 'Share/share',
+    })
+    console.log(e.detail.value);
+  }
+  },
+
+  startRecord:function(){
+    var that = this
+    console.log("stat record");
+    this.setData({
+      recording:true
+    })
+    console.log(this.data.recording);
+
+   wx.startRecord({
+     success: function (res) {
+       that.setData({
+       tempFilePath:res.tempFilePath
+     })
+     }
+   })
+
+  },
+ stopRecord:function(){
+   var that=this
+   console.log("stop record");
+   this.setData({recording: false})
+    wx.stopRecord({
+
+    })
+    that.setData({
+      hasRecord:true
+    })
+ },
+
+  playVoice: function () {
+   var that = this
+    var filePath=that.data.tempFilePath
+   console.log(filePath);
+   wx.playVoice({
+     filePath: filePath,
+   })
+    //that.saveFileToLocal()
+   methods.uploadFile(filePath)
+  //methods.downloadFile()
+  },
+
+  //播放网络返回的地址
+  playNetVoice:function(){
+   console.log("local file address "+this.data.filePath)
+   wx.playVoice({
+     filePath:this.data.filePath,
+   })
+  }
+  
+ 
+  
+})
